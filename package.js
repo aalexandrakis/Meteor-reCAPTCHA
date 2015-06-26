8d84a2d1c06e1da2d8a564866a3bf611767b13e0
Alexandrakis Alexandros <aalexandrakis@hotmail.com>
2015-06-26 14:47:51 +0300

    -- change version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: "aalexandrakis:recaptcha",
     summary: "Implementation of Google reCAPTCHA V2 for Meteor with language support",
     git: "https://github.com/aalexandrakis/Meteor-reCAPTCHA.git",
-    version: "2.0.6",
+    version: "2.0.7",
     license: "MIT"
 });
 
